da9cabef36d5ddc17ddbf39e3c3a9dcde59d04fe
Idra <percy.baker.0@gmail.com>
2021-06-09 22:10:36 +0100

    Updated version to 0.1.0 (all critical bugs fixed)

--- src/me/idra/multiblocksystem/managers/ManagerPlugin.java
+++ src/me/idra/multiblocksystem/managers/ManagerPlugin.java
@@ -22,7 +22,7 @@ public class ManagerPlugin {
 
 
 	
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.1.0";
 	public static final String AUTHOR = "Idra";
 
 	public static Plugin plugin;
