74376a813b049661fe2b70069b555a428594128c
jarkabaker <jarka.baker.1@gmail.com>
2021-06-14 23:14:22 +0100

    ListInformationScoreBoards added

---
+++ src/me/idra/multiblocksystem/lists/ListInformationScoreBoards.java
@@ -0,0 +1,26 @@
+package me.idra.multiblocksystem.lists;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
+import me.idra.multiblocksystem.objects.InformationScoreboard;
+
+
+public class ListInformationScoreBoards {
+
+	private ListInformationScoreBoards() {
+		// Empty constructor
+	}
+
+	// Map
+    public static Map<Player, InformationScoreboard> playerScoreBoard = new HashMap<>();
+	
+    public void updateScoreboard() {
+    	
+    	playerScoreBoard.forEach((key, value) -> value.update());
+    	
+    }
+
+}
