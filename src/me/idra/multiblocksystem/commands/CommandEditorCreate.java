6fa9e93ddd42c93effeec28f21e8e282558bb338
Idra <percy.baker.0@gmail.com>
2021-06-17 00:31:56 +0100

    Halted progress on editor for itemstack rewrite

--- src/me/idra/multiblocksystem/commands/CommandEditorCreate.java
+++ src/me/idra/multiblocksystem/commands/CommandEditorCreate.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.idra.multiblocksystem.bases.BaseCommand;
-import me.idra.multiblocksystem.objects.MixedItemStack;
+import me.idra.multiblocksystem.objects.AbstractMixedItemStack;
 
 
 
@@ -45,16 +46,20 @@ public class CommandEditorCreate extends BaseCommand{
 		Location corner = player_location.add(1, 0, 1);
 
 		// Build block array
-		List<List<List<MixedItemStack>>> stack_list = new ArrayList<> ();
+		List<List<List<String>>> stack_list = new ArrayList<> ();
 
 		for (int y = 0; y < dimension_y; y++) {
-			List<List<MixedItemStack>> stack = new ArrayList<> ();
+			List<List<AbstractMixedItemStack>> stack = new ArrayList<> ();
 
 			for (int x = 0; x < dimension_x; x++) {
-				List<MixedItemStack> stack_x = new ArrayList<> ();
+				List<AbstractMixedItemStack> stack_x = new ArrayList<> ();
 
 				for (int z = 0; z < dimension_z; z++) {
-					// Location loc = new 
+
+					Location location = corner.clone().add(x, y, z);
+					Block block = location.getBlock();
+					
+					// only god knows
 				}
 			}		
 		}
