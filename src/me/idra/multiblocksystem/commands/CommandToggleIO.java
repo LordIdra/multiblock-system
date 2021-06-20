6ee904ff6270ccc55f6b96f47f12cc2bb8d0dbd0
Young <andreasyoungandersen@gmail.com>
2021-06-20 19:02:33 +0200

    Toggling of Input/Output for Storages

---
+++ src/me/idra/multiblocksystem/commands/CommandToggleIO.java
@@ -0,0 +1,61 @@
+package me.idra.multiblocksystem.commands;
+
+import me.idra.multiblocksystem.bases.BaseCommand;
+import me.idra.multiblocksystem.bases.BaseWorldMultiblock;
+import me.idra.multiblocksystem.lists.ListWorldMultiblocks;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.util.BlockIterator;
+
+public class CommandToggleIO extends BaseCommand {
+	public CommandToggleIO() {
+		super();
+
+		name = new String[]{"toggle", "io"};
+		description = ChatColor.DARK_AQUA + "Toggles the inventory as either a Input or Output";
+		arguments = null;
+		hidden = false;
+		console = false;
+
+		addPermission();
+		addName();
+	}
+
+	@Override
+	public boolean commandFunction(CommandSender sender, Command command, String label, String[] args) {
+		Player player = (Player) sender;
+		BlockIterator block_iterator = new BlockIterator(player, 5);
+		BaseWorldMultiblock multi_block;
+
+
+		Inventory container_block = null;
+
+
+		while (block_iterator.hasNext()) { // While there are still elements in the block_iterator
+
+			Block next_block = block_iterator.next(); // Get the next element from block_iterator (does not move cursor forwards)
+
+			if (next_block.getState() instanceof Container) { // If block is a container
+				container_block = ((Container) next_block.getState()).getInventory();
+				break;
+			}
+		}
+		if (container_block == null || container_block.getLocation() == null) {
+			// TODO: Log that an INV has not been found
+			return false;
+		} else {
+			multi_block = ListWorldMultiblocks.getMultiblockFromLocation(container_block.getLocation());
+			if (multi_block == null) {
+				// TODO: Log that there was no MultiBlock structure found
+				return false;
+			}
+			multi_block.flipIO(container_block);
+		}
+		return true;
+	}
+}
