151482659f0ad52b0847afb08a704917280c3225
160044 <Fly.14.zelda@gmail.com>
2021-06-21 15:06:35 -0400

    starting work, slow progress

--- src/hyn/IsMultiblockLoaded/createMultiblockIsLoadedMap.java
+++ src/hyn/IsMultiblockLoaded/createMultiblockIsLoadedMap.java
@@ -7,8 +7,7 @@ public class createMultiblockIsLoadedMap {
 
 
 
-    // 2. Check the array of loaded Chunks against the map of all existing Multiblocks and add the ones that match the array to a new map [MultiblockIsLoaded], probably by copying the data from one map to the other (is it possible to add data regarding the chunk its main block is in, say what chunk the lectern is in?)
-    // 3. Check MultiblockIsLoadedMap for Multiblocks that do not exist in the loaded chunks array and remove them from the map
-    // 4. Pass MultiblockIsLoadedMap to the ticking routines
+    // 2. Check the array of loaded Chunks against the map of all existing Multiblocks and add the ones that match the array to a new array [MultiblockIsLoaded], probably by copying the data from one array to the other (is it possible to add data regarding the chunk its main block is in, say what chunk the lectern is in?)
+    // 3. Pass MultiblockIsLoadedArray to the ticking routines
 
 }
