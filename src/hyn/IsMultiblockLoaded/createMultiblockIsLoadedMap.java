98622a20bd3fadceceb847555a2c2471c2ff98cb
160044 <Fly.14.zelda@gmail.com>
2021-06-21 05:54:43 -0400

    its 0549, got into theorizing the loaded chunk check, anon, kill me later for lack of conventions

--- src/hyn/IsMultiblockLoaded/createMultiblockIsLoadedMap.java
+++ src/hyn/IsMultiblockLoaded/createMultiblockIsLoadedMap.java
@@ -3,7 +3,7 @@ package hyn.IsMultiblockLoaded;
 public class createMultiblockIsLoadedMap {
 
     //What this class needs to do:
-    //1. Create an array of loaded Chunks from method getLoadedChunks()
+    //1. Create an array of loaded Chunks from bukkit method getLoadedChunks()
     //2. Check the array of loaded Chunks against the map of all existing Multiblocks and add the ones that match the array to a new map [MultiblockIsLoaded], probably by copying the data from one map to the other (is it possible to add data regarding the chunk its main block is in, say what chunk the lectern is in?)
     //3. Check MultiblockIsLoadedMap for Multiblocks that do not exist in the loaded chunks array and remove them from the map
     //4. Pass MultiblockIsLoadedMap to the ticking routines
