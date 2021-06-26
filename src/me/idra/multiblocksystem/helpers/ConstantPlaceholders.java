d8f7a9348fc398e0399b3d54b16dd0a1de87a58e
Idra <percy.baker.0@gmail.com>
2021-06-26 22:01:20 +0100

    Started updating logger format in Bases

--- src/me/idra/multiblocksystem/helpers/ConstantPlaceholders.java
+++ src/me/idra/multiblocksystem/helpers/ConstantPlaceholders.java
@@ -2,15 +2,9 @@ package me.idra.multiblocksystem.helpers;
 
 public class ConstantPlaceholders {
 
-	public static final String PLAYER = "%player%";
-	public static final String NAME = "%name%";
-    public static final String MULTIBLOCK = "%multiblock%";
-    public static final String ID = "%id%";
-    public static final String RESEARCH = "%research%";
-    public static final String TYPE = "%type%";
-    public static final String FILE = "%file%";
-    public static final String PATH = "%path%";
-    public static final String TAG = "%tag%";
+	public static final String arg1 = "%arg1%";
+	public static final String arg2 = "%arg2%";
+    public static final String arg3 = "%arg3%";
 
     private ConstantPlaceholders() {
         // Empty constructor
